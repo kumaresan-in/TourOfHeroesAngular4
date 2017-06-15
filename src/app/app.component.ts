fb5e33bf79ed15f0f56ad6692bdd3252998e4d21
Kumaresan <kumaresh876@gmail.com>
2017-06-15 14:28:14 +0530

    First Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+             <h1>{{title}}</h1>
+             <h2>{{hero.name}}Details !!!</h2>
+             <div> <label> id:</label>{{hero.id}} </div>
+             <div>
+                  <label>name:</label>
+                  <input [(ngModel)]="hero.name" placeholder="Name">
+             </div>
+             `
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 }
