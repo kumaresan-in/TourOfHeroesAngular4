60a5000b05bd144a0b3df61e4a359779ddf6ee29
Kumaresan <kumaresh876@gmail.com>
2017-06-16 10:28:18 +0530

    Services Created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+ 
 import { Hero } from './hero';
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
-
+import { HeroService } from './hero.service';
+ 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   template: `
-            <h1>{{title}}</h1>
-            <h2>My Heroes</h2>
-            <ul class="heroes">
-              <li *ngFor="let hero of heroes"
-                [class.selected]="hero === selectedHero"
-                (click)="onSelect(hero)">
-                <span class="badge">{{hero.id}}</span> {{hero.name}}
-              </li>
-            </ul>
-            <div *ngIf="selectedHero">
-              <h2>{{selectedHero.name}} details!</h2>
-              <div><label>id: </label>{{selectedHero.id}}</div>
-              <div>
-                <label>name: </label>
-                <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-              </div>
-            </div>
-             `,
+    <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes"
+        [class.selected]="hero === selectedHero"
+        (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    <hero-detail [hero]="selectedHero"></hero-detail>
+  `,
   styles: [`
     .selected {
       background-color: #CFD8DC !important;
@@ -84,14 +65,25 @@ const HEROES: Hero[] = [
       margin-right: .8em;
       border-radius: 4px 0 0 4px;
     }
-  `]
+  `],
+  providers: [HeroService]
 })
-export class AppComponent {
-    title = 'Tour of Heroes';
-    heroes = HEROES;
-    selectedHero: Hero;
-  
-    onSelect(hero: Hero): void {
-      this.selectedHero = hero;
-    }
+export class AppComponent implements OnInit {
+  title = 'Tour of Heroes';
+  heroes: Hero[];
+  selectedHero: Hero;
+ 
+  constructor(private heroService: HeroService) { }
+ 
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+ 
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+ 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms'
+
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component'
+
 
 @NgModule({
   declarations: [
